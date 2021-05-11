d386c379d541e1c50a44428d8b584be0f7e03396
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-11 23:36:13 +0530

    Spelling correction done

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -2,6 +2,6 @@ package javapractice;
 
 public class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("Wellcome to Java Workld");
+		System.out.println("Wellcome to Java World");
 	}
 }
