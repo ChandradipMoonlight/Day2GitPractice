c638c490e738618c66d44c383690093899f33458
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-12 00:10:54 +0530

    done with code

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -3,6 +3,7 @@ package javapractice;
 public class HelloJava {
 	public static void main(String[] args) {
 
-		System.out.println("Wellcome to the Java world");
+		System.out.println("Wellcome to the Java World");
+		System.out.println("I am learnig gitHub commands");
 	}
 }
