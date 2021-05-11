ee6be11d7afe5c342f80bb38d93506a5c7e2e8bf
Chandradip Shivankar <chandradipshivankar@gmail.com>
2021-05-11 23:42:05 +0530

    Update HelloJava.java

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -2,6 +2,6 @@ package javapractice;
 
 public class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("Wellcome to the Java World");
+		System.out.println("Wellcome to the Java world");
 	}
 }
