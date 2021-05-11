1a5f3db374fa958388a027de7cc7eeb8e58b5c22
Chandradip Shivankar <chandradipshivankar@gmail.com>
2021-05-11 23:52:56 +0530

    Update HelloJava.java
    deleted one greeting massege

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -3,8 +3,6 @@ package javapractice;
 public class HelloJava {
 	public static void main(String[] args) {
 
-		System.out.println("Wellcome to the java World");
-
 		System.out.println("Wellcome to the Java world");
 	}
 }
